fb104e1b470523e45f6ee1574da213f49a126394
Dragoș Șerban <dragos4002@gmail.com>
2025-05-17 22:59:04 +0300

    Simplify database connection configuration by using DATABASE_URL

--- db/pool.js
+++ db/pool.js
@@ -4,11 +4,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
